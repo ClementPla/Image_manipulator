c9f066a2411975aab08ef7f599e4956ab0cd83d1
ClementPla <clement.playout@insa-lyon.fr>
2016-12-09 23:05:13 -0500

    Updates on comment defining the different constants used in the program

--- src/controller/constants.py
+++ src/controller/constants.py
@@ -1,12 +1,20 @@
-# Constant concerning warning
+"""
+This package defined all the differents constants used one or more time in the rest of the applications
+"""
+
+# Constants defining warnings messages
 
 EXISTING_IMAGE_LOADED = 0
 
 
-#
+# Constants defining image formats handling
 FORMAT_HANDLE = ['.BMP', '.GIF', '.PNG', '.PCX', '.PNM', '.JPG', '.JPEG', '.TIFF'] #Todo Complete the list based on openCV capacities
 
 
+
+
+
+#Frame aspects are defined by following constants:
 # Ratio is defined as (widget size)/(screen size)
 MAIN_PANEL_RATIO = 0.9
 
